13c4e8a26df97252730cba60f7ce4258c59677dd
Saif Al Jabry <68194253+saifprogram7@users.noreply.github.com>
2022-06-04 19:08:52 +0100

    Add files via upload

---
+++ lottery-generator.py
@@ -0,0 +1,6 @@
+from random import randint
+print("Welcome to the Lottery Generator! ")
+print("These numbers are randomly generated")
+print(input("Press Enter to get the winning lottery number!"))
+print(randint(1, 100), randint(1, 100), randint(1, 100), randint(1, 100), randint(1, 100), randint(1, 100))
+print(input())
